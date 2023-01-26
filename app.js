0803bb8c7cfaad8cd48af9a910afbc85ddcbbc82
payalpagaria <pagariapayal67@gmail.com>
2023-01-26 21:13:28 +0530

    update files

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app=express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.static('public'));
 app.get('/',(req,res)=>{
     let date=new Date();
     const option={
